72424f14ee54199f34657bb0e96f2f4ca2d4cb6d
LipaMar <48134943+LipaMar@users.noreply.github.com>
2021-01-29 21:35:34 +0100

    Add models (#3)

--- src/quiz_app/admin.py
+++ src/quiz_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quiz, Question, Choice
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Choice)
