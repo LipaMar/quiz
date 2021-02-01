e8d38edaafa0667dbaefe66ee3281476b9a44445
Szymon <piersiak.szymon@gmail.com>
2021-02-01 01:52:22 +0100

    registration of created HTML pages

--- src/quiz_app/urls.py
+++ src/quiz_app/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 
-app_name = 'Quiz wsiz'
+app_name = 'quiz'
 urlpatterns = [
 
     path('', views.index, name='index'),
+
+    path('<int:quiz_id>/', views.single_quiz_page, name='single_quiz_page'),
+    path('<int:quiz_id>/<int:question_id>/', views.single_question_page, name='single_question_page'),
+    path('<int:quiz_id>/<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:quiz_id>/results', views.results_page, name='results_page'),
 
 
 ]
