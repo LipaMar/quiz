5c41808bde8dba1f11f762f8c3dfd96ac7ebdbfe
LipaMar <48134943+LipaMar@users.noreply.github.com>
2021-02-06 18:37:23 +0100

    App (#9)
    update branch

---
+++ src/quiz_app/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+class CreateNewQuizForm(forms.Form):
+    quiz_name = forms.CharField(max_length=30, label="Quiz Name")
+    num_questions = forms.IntegerField(label="Number of Questions")
+
+    def clean_name(self):
+        data = self.cleaned_data['quiz_name']
+
+        if data == "Default_Name":
+            raise ValidationError(_('Invalid name! Quiz name must be unique.'))
+
+        return data
+
+    def clean_num_questions(self):
+        data = self.cleaned_data['num_questions']
+
+        if (isinstance(data, int) is False) or (data < 1):
+            raise ValidationError(_('Invalid number of questions! Must be positive integer.)'))
+
+        return data
+
+
+class CreateNewQuestionForm(forms.Form):
+    question_text = forms.CharField(max_length=300, label="Question Text",
+                                    widget=forms.TextInput(attrs={'class': 'question_text_box'}))
+
+    choice1_text = forms.CharField(max_length=300, label="Choice 1",
+                                   widget=forms.TextInput(attrs={'class': 'choice_box'}))
+    choice1_correctness = forms.BooleanField(label="Choice 1 Correct?", required=False,
+                                             widget=forms.CheckboxInput(attrs={'class': 'choice_correct_box'}))
+
+    choice2_text = forms.CharField(max_length=300, label="Choice 2",
+                                   widget=forms.TextInput(attrs={'class': 'choice_box'}))
+    choice2_correctness = forms.BooleanField(label="Choice 2 Correct?", required=False,
+                                             widget=forms.CheckboxInput(attrs={'class': 'choice_correct_box'}))
+
+    choice3_text = forms.CharField(max_length=300, label="Choice 3",
+                                   widget=forms.TextInput(attrs={'class': 'choice_box'}))
+    choice3_correctness = forms.BooleanField(label="Choice 3 Correct?", required=False,
+                                             widget=forms.CheckboxInput(attrs={'class': 'choice_correct_box'}))
+
+    choice4_text = forms.CharField(max_length=300, label="Choice 4",
+                                   widget=forms.TextInput(attrs={'class': 'choice_box'}))
+    choice4_correctness = forms.BooleanField(label="Choice 4 Correct?", required=False,
+                                             widget=forms.CheckboxInput(attrs={'class': 'choice_correct_box'}))
+
+    def clean(self):
+
+        choice1_answer = self.cleaned_data["choice1_correctness"]
+        choice2_answer = self.cleaned_data["choice2_correctness"]
+        choice3_answer = self.cleaned_data["choice3_correctness"]
+        choice4_answer = self.cleaned_data["choice4_correctness"]
+
+        answer_list = [choice1_answer, choice2_answer, choice3_answer, choice4_answer]
+
+        print(answer_list)
+
+        trueCount = 0
+        for answer in answer_list:
+            if answer:
+                trueCount += 1
+
+        if trueCount != 1:
+            raise ValidationError(_('Must have exactly one correct answer'))
+
+        return self.cleaned_data
--- src/quiz_app/views.py
+++ src/quiz_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Quiz, Choice
+from .models import Quiz, Choice, Question
+from .forms import CreateNewQuizForm, CreateNewQuestionForm
 
 
 def index(request):
@@ -10,11 +11,12 @@ def index(request):
     context = {'all_quizzes': all_quizzes}
     return render(request, 'index.html', context)
 
+
 def single_quiz_page(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     num_of_questions = len(quiz.question_set.all())
 
-    if(num_of_questions == 0):
+    if num_of_questions == 0:
         quiz.delete()
         all_quizzes = Quiz.objects.all()
         context = {'all_quizzes': all_quizzes}
@@ -29,6 +31,7 @@ def single_quiz_page(request, quiz_id):
     context = {'quiz': quiz, 'num_questions': num_of_questions}
     return render(request, 'single_quiz_page.html', context)
 
+
 def single_question_page(request, quiz_id, question_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     current_question = quiz.question_set.get(question_num=question_id)
@@ -39,7 +42,7 @@ def single_question_page(request, quiz_id, question_id):
         last_question_check = True
         next_or_submit = "Submit"
 
-    next_question_id = question_id+1
+    next_question_id = question_id + 1
 
     all_choices = current_question.choice_set.all()
 
@@ -60,7 +63,7 @@ def vote(request, quiz_id, question_id):
     current_question = quiz.question_set.get(question_num=question_id)
 
     next_or_submit = "Next"
-    if(question_id == (len(quiz.question_set.all()))):
+    if question_id == (len(quiz.question_set.all())):
         next_or_submit = "Submit"
 
     try:
@@ -75,15 +78,15 @@ def vote(request, quiz_id, question_id):
     else:
         correct_answer = current_question.choice_set.get(correct=True)
 
-        if(selected_choice == correct_answer):
+        if selected_choice == correct_answer:
             request.session["num_correct"] += 1
         else:
             request.session["num_wrong"] += 1
 
-        if(question_id == (len(quiz.question_set.all()))):
-            return HttpResponseRedirect(reverse('quiz:results_page', args=(quiz.id, )))
+        if question_id == (len(quiz.question_set.all())):
+            return HttpResponseRedirect(reverse('quiz:results_page', args=(quiz.id,)))
         else:
-            return HttpResponseRedirect(reverse('quiz:single_question_page', args=(quiz_id, question_id+1)))
+            return HttpResponseRedirect(reverse('quiz:single_question_page', args=(quiz_id, question_id + 1)))
 
 
 def results_page(request, quiz_id):
@@ -94,18 +97,96 @@ def results_page(request, quiz_id):
 
     total_questions = num_correct + num_wrong
 
-    score = num_correct/total_questions
+    score = num_correct / total_questions
     score_over_75 = False
-    if(score >= .75):
+    if score >= .75:
         score_over_75 = True
     percentage_score = "{:.0%}".format(score)
 
-    context ={
+    context = {
         'num_correct': num_correct,
-        'num_wrong':num_wrong,
+        'num_wrong': num_wrong,
         'score_over_75': score_over_75,
         'percentage_score': percentage_score,
         'total_questions': total_questions,
         'quiz': quiz,
     }
     return render(request, 'results_page.html', context)
+
+
+def create_quiz_page(request):
+    if request.method == 'POST':
+
+        form = CreateNewQuizForm(request.POST)
+
+        if form.is_valid():
+            quiz_name = form.cleaned_data['quiz_name']
+            num_questions = form.cleaned_data['num_questions']
+
+            new_quiz = Quiz(quiz_title=quiz_name, num_questions=num_questions)
+            new_quiz.save()
+
+            return HttpResponseRedirect(reverse('quiz:create_question_page', args=(new_quiz.id, 1,)))
+
+    else:
+        form = CreateNewQuizForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'create_quiz_page.html', context)
+
+
+def create_new_question_page(request, quiz_id, question_id):
+    quiz = Quiz.objects.get(pk=quiz_id)
+
+    if request.method == 'POST':
+
+        form = CreateNewQuestionForm(request.POST)
+
+        if form.is_valid():
+
+            question_text = form.cleaned_data['question_text']
+
+            choice1 = form.cleaned_data["choice1_text"]
+            choice1_correctness = form.cleaned_data["choice1_correctness"]
+
+            choice2 = form.cleaned_data["choice2_text"]
+            choice2_correctness = form.cleaned_data["choice2_correctness"]
+
+            choice3 = form.cleaned_data["choice3_text"]
+            choice3_correctness = form.cleaned_data["choice3_correctness"]
+
+            choice4 = form.cleaned_data["choice4_text"]
+            choice4_correctness = form.cleaned_data["choice4_correctness"]
+
+            question = Question(quiz=quiz, question_text=question_text, question_num=question_id)
+            question.save()
+
+            question.choice_set.create(choice_text=choice1, correct=choice1_correctness)
+            question.choice_set.create(choice_text=choice2, correct=choice2_correctness)
+            question.choice_set.create(choice_text=choice3, correct=choice3_correctness)
+            question.choice_set.create(choice_text=choice4, correct=choice4_correctness)
+
+            if question_id == quiz.num_questions:
+                return HttpResponseRedirect(reverse('quiz:index'))
+            else:
+                return HttpResponseRedirect(reverse('quiz:create_question_page', args=(quiz_id, question_id + 1,)))
+
+    else:
+        form = CreateNewQuestionForm()
+
+    if question_id == quiz.num_questions:
+        next_submit = "Submit"
+    else:
+        next_submit = "Next"
+
+    context = {
+        'form': form,
+        'question_num': question_id,
+        'next_submit': next_submit,
+
+    }
+
+    return render(request, 'create_question_page.html', context)
